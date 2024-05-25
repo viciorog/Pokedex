e9d7caee9bfe33d8db5a4e039703512d00ed8b9d
viciorog <148492158+viciorog@users.noreply.github.com>
2024-05-25 16:40:19 +0200

    Delete next-pokedex directory

--- next-pokedex/README.md
+++
@@ -1,1 +0,0 @@
-This is my Pokedex, hope you like it!
--- next-pokedex/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["w.wallhaven.cc", "images.pokemontcg.io"],
-  },
-};
-
-export default nextConfig;
